2d036a6e3fa1cc7dd94f76484f56b0a76c80b573
evgenkostrov <evgenkostrov@gmail.com>
2021-11-06 14:01:33 +0300

    some fix bars

--- app/src/main/java/com/example/turbo/MainActivity.kt
+++ app/src/main/java/com/example/turbo/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.turbo.databinding.ActivityMainBinding
@@ -21,63 +22,62 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
     lateinit var binding: ActivityMainBinding
-    lateinit var navController:NavController
+    lateinit var toggle: ActionBarDrawerToggle
     lateinit var bottom:BottomNavigationView
-
+    lateinit var navController:NavController
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        toggle=ActionBarDrawerToggle(this,binding.drawerLayout, R.string.open, R.string.close )
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        /**
+         *       Settings drawer
+         */
+
+        toggle=ActionBarDrawerToggle(this,binding.drawerLayout, R.string.open, R.string.close )
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        setOf(R.id.view1Fragment,R.id.view3Fragment,R.id.view3Fragment)
+        val appBarConfiguration= AppBarConfiguration(navGraph = navController.graph,drawerLayout = binding.drawerLayout)
+        setupActionBarWithNavController(navController,appBarConfiguration)
 
         binding.navView.setNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.item1 -> Toast.makeText(applicationContext,
+                R.id.view1Fragment-> {Toast.makeText(applicationContext,
                     "Clicked item 1",Toast.LENGTH_SHORT).show()
-                R.id.item2 -> Toast.makeText(applicationContext,
+                navController.navigate(R.id.view1Fragment)}
+                R.id.view2Fragment -> {Toast.makeText(applicationContext,
                     "Clicked item 2",Toast.LENGTH_SHORT).show()
-                R.id.item3 -> Toast.makeText(applicationContext,
+                navController.navigate(R.id.view2Fragment)}
+                R.id.view3Fragment -> {Toast.makeText(applicationContext,
                     "Clicked item 3",Toast.LENGTH_SHORT).show()
+                    navController.navigate(R.id.view3Fragment)}
             }
             true
         }
- bottom=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        navController = navHostFragment.findNavController()
-        bottom.setupWithNavController(navController)
 
+        /**
+         *       Settings bottom
+         */
+
+        bottom=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottom.setupWithNavController(navController)
 //        binding.bottomNavigationView.background = null
 //        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
-//        binding.bottomNavigationView.menu.getItem(1).isEnabled = true
-//
-        val badge = binding.bottomNavigationView.getOrCreateBadge(R.id.miHome)
-        var count=1
-        fun countMe():String{return count++.toString()}
-        binding.fab.setOnClickListener{
-                view->
-            Snackbar.make(view,"Here", Snackbar.LENGTH_LONG)
-                .setAction("Action"){count-1}
-                .show()
-            binding.tv.text="${badge.isVisible}"
-            binding.tv.append(countMe())}
-
- bottom.setOnItemSelectedListener {     item ->
+        bottom.setOnItemSelectedListener {     item ->
             when(item.itemId){
 
                 R.id.miHome -> {
                     binding.tv.text = "Home"
-                   true
+                    true
                 }
                 R.id.view1Fragment -> {
                     binding.tv.text = "Search"
@@ -98,9 +98,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        /**
+         *       Settings fab
+         */
 
-
-
+        val badge = binding.bottomNavigationView.getOrCreateBadge(R.id.miHome)
+        var count=1
+        fun countMe():String{return count++.toString()}
+        binding.fab.setOnClickListener{
+                view->
+            Snackbar.make(view,"Here", Snackbar.LENGTH_LONG)
+                .setAction("Action"){count-1}
+                .show()
+            binding.tv.text="${badge.isVisible}"
+            binding.tv.append(countMe())}
 
         badge.badgeGravity= BadgeDrawable.TOP_START
         badge.backgroundColor= Color.GREEN
@@ -120,13 +131,10 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(toggle.onOptionsItemSelected(item)){
             return true
         }
-
         return super.onOptionsItemSelected(item)
     }
 }
